add4c893079525df83c0e3bc33847bdd01694bb3
david_bille <david.bille@data-aces.com>
2021-08-06 09:36:45 -0500

    package updates

--- src/components/ResultsGrid.js
+++ src/components/ResultsGrid.js
@@ -18,11 +18,14 @@ export const ResultsGrid = ({data}) => {
         { headerName: 'Brand TRxEQ', field: 'brandTRxEQ' },
         { headerName: 'Brand TRxEQ/1000 Lives', field: 'brandTRxEQLives' },
         { headerName: 'Brand TRxEQ % Change', field: 'brandTRxEQChange' },
-        { headerName: 'Reject Rate', field: 'rejectRate' }
+        { headerName: 'Reject Rate', field: 'rejectRate' },
+        { headerName: 'Reversal Rate', field: 'reversalRate' },
+        { headerName: 'OPC Ask', field: 'opcAsk' },
+        { headerName: 'OPC Paid', field: 'opcPaid' },
       ],
       rowData: [
-        {payer: "Cigna - Commercial", enterpriseBenType: "Express Scripts - Commercial", pbm: "Express Scripts",coverage: "Tier 3", pa:"N", st:"N", lives:7133760, liveShareAllPlans:'2.37%',marketTRxEQ:85428, brandTRxEQ:1268, brandTRxEQLives: 0.18, brandTRxEQChange: 0.18, rejectRate:'4.01%' },
-        {payer: "CVS Health Administered Plans", enterpriseBenType: "CVS Health - Commercial", pbm: "CVS Health",coverage: "Tier 2", pa:"N", st:"N", lives:7133760, liveShareAllPlans:'2.37%',marketTRxEQ:85428, brandTRxEQ:1268, brandTRxEQLives: 0.18, brandTRxEQChange: 0.18, rejectRate:'4.01%' }
+        {payer: "Cigna - Commercial", enterpriseBenType: "Express Scripts - Commercial", pbm: "Express Scripts",coverage: "Tier 3", pa:"N", st:"N", lives:7133760, liveShareAllPlans:'2.37%',marketTRxEQ:85428, brandTRxEQ:1268, brandTRxEQLives: 0.18, brandTRxEQChange: 0.18, rejectRate:'4.01%', reversalRate: '3.04%', opcAsk: '$50.00', opcPaid: '$50.00' },
+        {payer: "CVS Health Administered Plans", enterpriseBenType: "CVS Health - Commercial", pbm: "CVS Health",coverage: "Tier 2", pa:"N", st:"N", lives:7133760, liveShareAllPlans:'2.37%',marketTRxEQ:85428, brandTRxEQ:1268, brandTRxEQLives: 0.18, brandTRxEQChange: 0.18, rejectRate:'4.01%', reversalRate: '3.04%', opcAsk: '$50.00', opcPaid: '$50.00' }
       ]
   }); 
   return (
