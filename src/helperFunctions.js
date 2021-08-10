842e9d27ead65088d4f59d79a5f789217de2de46
david_bille <david.bille@data-aces.com>
2021-08-10 13:04:00 -0500

    updated validation logic to satisfy console warning message

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,23 +1,35 @@
 export const parameterValidations = (parameters) => {
+  // ensures user has selected a query engine
   if (parameters.engine === null) {
     window.alert('Please select a processing engine...')
     return false
   }
+  // ensures user has selected a market
   if (parameters.market === null) {
     window.alert('Please select a market...')
     return false
   }
+  // ensures user has selected a brand
   if (parameters.brand === null) {
     window.alert('Please select a brand...')
     return false
   }
+  // ensures user has selected a current date range
   if (parameters.currStartDate === null || parameters.currEndDate === null) {
     window.alert('Please select a valid date range for current period')
     return false
   }
-  if (parameters.prevStartDate !== null && parameters.prevEndDate === null || parameters.prevStartDate === null && parameters.prevEndDate !== null) {
-    window.alert('Please select a valid date range for prior period')
-    return false
+  // ensures user has selected both 'prev' date ranges if one has been selected 
+  if (parameters.prevStartDate !== null)  {
+    if (parameters.prevEndDate === null) {
+      window.alert('Please select a valid date range for prior period')
+      return false
+    }
+  } else if (parameters.prevStartDate === null) {
+    if (parameters.prevEndDate !== null) {
+      window.alert('Please select a valid date range for prior period')
+      return false
+    }
   }
   return true
 }
